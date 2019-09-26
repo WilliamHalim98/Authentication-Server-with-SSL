e519478a3835deea1b158b4a5d5cb6f9eeb1f493
root <root@LAPTOP-QLDIOAH3.localdomain>
2019-09-26 22:45:49 +0700

    Second Commit-Revised Code

--- 18217021_WilliamHalim_TugasSSL.py
+++ 18217021_WilliamHalim_TugasSSL.py
@@ -1,67 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 #Nama : William Halim
 #NIM  : 18217021
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import base64
 import ssl
 
-class CustomHandler(BaseHTTPRequestHandler):
-    def do_HEAD(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+USERS = [
+    {
+        "username": "test",
+        "password": "test",
+        "hash": base64.b64encode("test:test".encode('utf-8')).decode('utf-8')
+    }
+]
 
-    def do_AUTHHEAD(self):
+
+class AuthHandler(BaseHTTPRequestHandler):
+    def require_auth(self):
         self.send_response(401)
-        self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
-        self.send_header('Content-type', 'text/html')
+        self.send_header("WWW-Authenticate", 'Basic realm="Tugas SSL\r\n\r\n"')
         self.end_headers()
 
-    def do_GET(self) : 
-        if self.headers['Authorization'] == None:
-            self.do_AUTHHEAD()
-            self.wfile.write(bytes('no auth header received', 'UTF-8'))
-            pass
-        elif self.headers['Authorization'] == 'Basic YW5vdGhlcjptZQ==':
-            self.do_HEAD()
-            self.wfile.write(bytes(self.headers['Authorization'], 'UTF-8'))
-            self.wfile.write(bytes(' authenticated!', 'UTF-8'))
-            pass
-        else:
-            self.do_AUTHHEAD()
-            self.wfile.write(bytes(self.headers['Authorization'], 'UTF-8'))
-            self.wfile.write(bytes(' not authenticated', 'UTF-8'))
-            pass
+        
+    def do_GET(self):
+        auth = self.headers['Authorization']
+        if not auth:
+            return self.require_auth()
 
-def main():
-    listen_target = ('192.168.5.105', 10001) #192.168.5.105 adalah private ip address dari laptop saya
-    certificate_file = '/certificate.pem'
-    private_key_file = '/privkey.pem'
-    try:
-        httpd = HTTPServer(listen_target, CustomHandler)
-        httpd.socket = ssl.wrap_socket(httpd.socket,certfile=certificate_file, keyfile=private_key_file, server_side=True)
-        print ('started httpd...')
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print ('^C received, shutting down server')
+        hash_val = auth.split(' ')[1]
+        for user in USERS:
+            if user['hash'] == hash_val:
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write("<html><body>Hi, {}!</body></html>".format(user['username']).encode('utf-8'))
+                return
 
-if __name__ == '__main__':
-    main()
+        return self.require_auth()
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+if __name__ == "__main__":
+    server_address = ('localhost', 10100) 
+    httpd = HTTPServer(server_address, AuthHandler)
+    httpd.socket = ssl.wrap_socket (httpd.socket, 
+        keyfile="/privkey.pem", 
+        certfile='/certificate.pem', server_side=True)
+    httpd.serve_forever()
